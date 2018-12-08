0e2de839f1845f728ecd755a96297b8844192e7c
MinhHieuVenRoM <44973205+MinhHieuVenRoM@users.noreply.github.com>
2018-12-08 19:00:23 +0700

    Revert "Revert "Hieu""
    This reverts commit fce95e110356b86670917e3c21cf73b20ae1658c.

---
+++ Doanhieu/src/Model/LOAISANPHAMModel.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Control.MSSQLControl;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Minh Hieu
+ */
+public class LOAISANPHAMModel {
+
+    private String MALOAI;
+    private String TENLOAI;
+    private ArrayList<LOAISANPHAMModel> Dsloai;
+    private static Connection connection;
+
+    public String getMALOAI() {
+        return MALOAI;
+    }
+
+    public String getTENLOAI() {
+        return TENLOAI;
+    }
+
+    public ArrayList<LOAISANPHAMModel> getDsloai() {
+        return Dsloai;
+    }
+
+    public void setMALOAI(String MALOAI) {
+        this.MALOAI = MALOAI;
+    }
+
+    public void setTENLOAI(String TENLOAI) {
+        this.TENLOAI = TENLOAI;
+    }
+
+    public void setDsloai(ArrayList<LOAISANPHAMModel> Dsloai) {
+        this.Dsloai = Dsloai;
+    }
+
+    public void layDanhsachloaisanpham() throws SQLException {
+        connection = MSSQLControl.getConnect();
+        Statement statement = connection.createStatement();
+        String sql = "select * from LOAISANPHAM";
+        ResultSet resultSet = statement.executeQuery(sql);
+        Dsloai = new ArrayList<>();
+
+        while (resultSet.next()) {
+            LOAISANPHAMModel loai = new LOAISANPHAMModel();
+            loai.setMALOAI(resultSet.getString("MALOAI"));
+            loai.setTENLOAI(resultSet.getString("TENLOAI"));
+            Dsloai.add(loai);
+        }
+        connection.close();
+    }
+
+    public int demSoloaisanpham() {
+        int i = 0;
+        for (LOAISANPHAMModel loai : Dsloai) {
+            i++;
+        }
+        return i;
+    }
+}
